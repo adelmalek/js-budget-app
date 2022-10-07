12078d62d3182d8eaf9fb51dc3c39d9d9fe6b1bc
Adel <gerencser.adel@gmail.com>
2022-10-07 13:04:15 +0200

    data storage

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
-let budget = (() => {
-    let a = 10;
-    let sum = (b) => {
-        return a + b;
-    }
+const addButton = document.querySelector("#add-btn");
+const plusOrMinus = document.querySelector("#select");
+const description = document.querySelector("#add-description");
+const money = document.querySelector("#add-value");
 
-    return {
-        test: (c) => {
-            return sum(c)
+const budgetValue = document.querySelector("#budget-value");
+const budgetIncomeValue = document.querySelector("#budget-income-value");
+const budgetExpenditureValue = document.querySelector("#budget-expenditure-value");
+const budgetExpenditurePercent = document.querySelector("#budget-expenditure-percent");
+
+
+let budget = (() => {
+    let Incomes = function(id, des, val) {
+        this.id = is;
+        this.des = des;
+        this.val = val
+    };
+
+    let Expenditures = function(id, des, val) {
+        this.id = id;
+        this.des = des;
+        this.val = val;
+    };
+
+    let datas = {
+        descriptions: {
+            allIncomes: [],
+            allExpenditures: []
+        },
+        values: {
+            allIncomes: [],
+            allExpenditures: []
         }
-    }
+    };
+
 })();
+
 
 let userInterface = (() => {
-
-})();
-
-let control = ((bud, ui) => {
-    let num = bud.test(20);
-    
     return {
-        test: () => {
-            console.log(num);
+        getInput: () => {
+            return {
+                type: plusOrMinus.value,
+                dec: description.value,
+                value: money.value
+            }
         }
     }
-})(budget, userInterface);
+})();
 
-control.test();
+
+let control = ((bud, ui) => {
+    addButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        let input = ui.getInput();
+        console.log(input);
+    })
+})(budget, userInterface);
