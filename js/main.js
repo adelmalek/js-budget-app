41d494b9e92703deeed0cff52cea9cec1acd7fe9
Adel <gerencser.adel@gmail.com>
2022-10-11 13:04:00 +0200

    display datas

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ const budgetIncomeValue = document.querySelector("#budget-income-value");
 const budgetExpenditureValue = document.querySelector("#budget-expenditure-value");
 const budgetExpenditurePercent = document.querySelector("#budget-expenditure-percent");
 
+const incomeList = document.querySelector("#income-list");
+const expenditureList = document.querySelector("#expenditure-list");
+
 
 let budget = (() => {
     let Incomes = function(id, des, val) {
@@ -44,9 +47,9 @@ let budget = (() => {
                 id = 0;
             }
 
-            if (type === "plus") {
+            if (type === "plus" &&  desc.trim().length > 0) {
                 newItem = new Incomes(id, desc, val);
-            } else if (type === "minus") {
+            } else if (type === "minus" && desc.trim().length > 0) {
                 newItem = new Expenditures(id, desc, val);
             }
 
@@ -70,6 +73,39 @@ let userInterface = (() => {
                 dec: description.value,
                 value: money.value
             }
+        },
+        displayItems: (obj, type) => {
+            if (type === "plus") {
+                incomeList.innerHTML += `
+                    <div class="item" id="incomes-${obj.id}">
+                        <div class="item-description">${obj.des}</div>
+                        <div class="right">
+                            <div class="item-value">${obj.val} &euro;</div>
+                            <div class="item-delete">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M18.05 33.05 24 27l6 6.05 2.35-2.4-5.95-6.05 5.95-6.05-2.35-2.4-6 6.05-5.95-6.05-2.4 2.4 6 6.05-6 6.05Zm-5 8.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-21.9 0V39Z"/></svg>
+                            </div>
+                        </div>
+                    </div>
+                `
+            } else if (type === "minus") {
+                expenditureList.innerHTML += `
+                    <div class="item" id="expenditures-${obj.id}">
+                        <div class="item-description">${obj.des}</div>
+                        <div class="right">
+                            <div class="item-value">${obj.val} &euro;</div>
+                            <div class="item-percent">21%</div>
+                            <div class="item-delete">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M18.05 33.05 24 27l6 6.05 2.35-2.4-5.95-6.05 5.95-6.05-2.35-2.4-6 6.05-5.95-6.05-2.4 2.4 6 6.05-6 6.05Zm-5 8.95q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-21.9 0V39Z"/></svg>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }
+        },
+        initInputField: function() {
+            description.value = "";
+            money.value = "";
+            description.focus();
         }
     }
 })();
@@ -80,5 +116,7 @@ let control = ((bud, ui) => {
         e.preventDefault();
         let input = ui.getInput();
         let newItem = bud.addItems(input.type, input.dec, input.value);
+        ui.displayItems(newItem, input.type);
+        ui.initInputField();
     })
 })(budget, userInterface);
